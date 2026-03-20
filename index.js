1ad47d0cdefa2afe62215604c6be4cd9b411a7dd
Aniketgupta4 <aniketkumargupta302@gmail.com>
2026-03-20 12:21:56 +0530

    gms

--- index.js
+++ index.js
@@ -48,17 +48,12 @@ if (process.env.NODE_ENV === "production") {
     // 🔹 Serve React build
     app.use(express.static(path.join(__dirname1, "gms-frontend", "build")));
 
-    app.get("*", (req, res) => {
-  if (
-    !req.path.startsWith("/auth") &&
-    !req.path.startsWith("/plans") &&
-    !req.path.startsWith("/members")
-  ) {
-    res.sendFile(
-      path.resolve(__dirname1, "gms-frontend", "build", "index.html")
-    );
-  }
-});
+    // 🔥 Express 5 FIX (no crash)
+    app.get(/.*/, (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname1, "gms-frontend", "build", "index.html")
+        );
+    });
 
 } else {
 
